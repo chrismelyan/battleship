edb0df8f46d2c420b3e3ab33b63faad265fd9dfb
chrismelyan <chris.shelpuk@gmail.com>
2022-01-06 20:11:07 +0100

    if

--- JS-projects/script.js
+++ JS-projects/script.js
@@ -1,25 +1,35 @@
-/* Задание на урок:
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}; 
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+          b = prompt('На сколько оцените его?', '');
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMoviesDB.movies[a] = b;
+        console.log("Done");
+    } else {
+        console.log('error');
+        i--;
     }
+}
 
-Проверить, чтобы все работало без ошибок в консоли */
+if (personalMoviesDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMoviesDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 
-'use strict';
+console.log(personalMoviesDB);
 
